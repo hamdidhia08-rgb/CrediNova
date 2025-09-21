161abd93bf23fa22e72a8a2f44cbc2c5da9a24c1
kabil <kabilfouzai'@gmail.com>
2025-09-21 18:32:43 +0200

    test

--- Components/Bottombar/Bottombar.tsx
+++ Components/Bottombar/Bottombar.tsx
@@ -5,29 +5,30 @@ import { HomeIcon, CubeIcon, CreditCardIcon } from "@heroicons/react/24/outline"
 
 export default function MobileBottomBar() {
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-gray-900 border-t border-gray-700 shadow-inner z-50 sm:hidden">
-      <div className="flex justify-around items-center h-16 px-4">
-        {/* Home */}
-        <Link href="/dashboard/home" className="flex flex-col items-center text-gray-400 hover:text-blue-500 transition">
-          <HomeIcon className="h-6 w-6" />
-          <span className="text-xs font-medium mt-1">Home</span>
-        </Link>
+   <nav className="fixed bottom-0 left-0 w-full bg-gray-900 border-t border-gray-700 shadow-inner z-50 sm:hidden">
+  <div className="relative flex justify-around items-center h-16 px-4">
+    {/* Extra Design */}
+    <div className="absolute top-0 left-0 right-0 mx-auto w-16 h-1 rounded-full bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 shadow-lg"></div>
 
-        {/* My Pack */}
-        <Link href="/dashboard/pack" className="flex flex-col items-center text-gray-400 hover:text-yellow-400 transition">
-          <CubeIcon className="h-6 w-6" />
-          <span className="text-xs font-medium mt-1">My Pack</span>
-        </Link>
+    {/* Home */}
+    <Link href="/dashboard/home" className="flex flex-col items-center text-gray-400 hover:text-blue-500 transition z-10">
+      <HomeIcon className="h-6 w-6" />
+      <span className="text-xs font-medium mt-1">Home</span>
+    </Link>
 
-        {/* My Cards */}
-        <Link href="/dashboard/cards" className="flex flex-col items-center text-gray-400 hover:text-green-400 transition">
-          <CreditCardIcon className="h-6 w-6" />
-          <span className="text-xs font-medium mt-1">My Cards</span>
-        </Link>
-      </div>
+    {/* My Pack */}
+    <Link href="/dashboard/pack" className="flex flex-col items-center text-gray-400 hover:text-yellow-400 transition z-10">
+      <CubeIcon className="h-6 w-6" />
+      <span className="text-xs font-medium mt-1">My Pack</span>
+    </Link>
 
-      {/* Extra Design */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-16 h-1 rounded-full bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 shadow-lg"></div>
-    </nav>
+    {/* My Cards */}
+    <Link href="/dashboard/cards" className="flex flex-col items-center text-gray-400 hover:text-green-400 transition z-10">
+      <CreditCardIcon className="h-6 w-6" />
+      <span className="text-xs font-medium mt-1">My Cards</span>
+    </Link>
+  </div>
+</nav>
+
   );
 }
